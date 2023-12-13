8e31bd18ef4beebda64bb3a7e9cdc4ade7cb9add
ishanstga <ishanstga@gmail.com>
2023-12-13 12:19:57 +0530

    Cleanup Code

--- Assistant.py
+++ Assistant.py
@@ -3,8 +3,7 @@ from gpt4all import GPT4All
 import whisper
 import os
 import pyttsx3
-
-
+from time import sleep
 import elevenlabs
 
 #test text to voice------------------------------------------------------------------------------------------
@@ -23,18 +22,15 @@ audio_array = generate_audio(text_prompt)
 '''
 #------------------------------------------------------------------------------------------------------------
 
-#Openai whisper voice recognition model paths
 base_model_path = os.path.expanduser('C:/Users/ASUS/.cache/whisper/base.pt')
 tiny_model_path = os.path.expanduser('C:/Users/ASUS/.cache/whisper/tiny.pt')
-model = whisper.load_model(tiny_model_path) #load methord
+model = whisper.load_model(base_model_path)
 
-#gpt4all falcon model path
 gptModel = GPT4All("E:\Projects\Assistant\AI Models\gpt4all-falcon-q4_0.gguf", allow_download=False)
 
-#varriable for recognize audio input
 listener = sr.Recognizer()
+mic = sr.Microphone()
 
-#varriable for text to speech
 engine = pyttsx3.init()
 engine.setProperty('rate',145)
 voices = engine.getProperty('voices')
@@ -42,48 +38,53 @@ engine.setProperty('voice', voices[0].id)
 
 A = open("APIkey.txt", "r")
 API = A.read()
-#elevenlabs.set_api_key(API)
+elevenlabs.set_api_key(API)
 
 wake_word = "alex"
 
-#Text to speech function
-def speek(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-#get prompt using whisper in offline
-def listen_for_wake_word(audio):
-    with open("english.wav", "wb") as f:
-        f.write(audio.get_wav_data())
-        result = model.transcribe("english.wav", language= 'en', fp16=False)
-        print(result["text"])
-        promptText = result["text"]
-    return promptText
-
-def gptPrompt(text):
-    output = gptModel.generate(text, max_tokens=200)
-    print(output)
-    #speek(output)
+def elevanlabs(text):
     naturalvoice = elevenlabs.Voice(voice_id="XB0fDUnXU5powFXDhCwa", settings=elevenlabs.VoiceSettings(stability=1,similarity_boost=0.95))
-    audio = elevenlabs.generate(text=output,voice=naturalvoice)
+    audio = elevenlabs.generate(text=text,voice=naturalvoice)
     elevenlabs.play(audio)
 
-def  take_command():
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def start_listening():
     try:
-        with sr.Microphone() as source:
+        with mic as source:
             listener.adjust_for_ambient_noise(source, duration=2)
             print("listening...")
-            voice = listener.listen(source)
-            prompt = listen_for_wake_word(voice)
-            prompt = prompt.lower()
-            if wake_word in prompt:
-                gptPrompt(prompt)
-            else:
-                print(wake_word + " is not in command")
-            
+            uservoice = listener.listen(mic) 
+            return uservoice
     except:
-        print("exept pass")
+        print("Mic error")
         pass
 
+def get_user_command_as_text(voice):
+    # Convert Voice To Text
+    with open("english.wav", "wb") as f:
+        f.write(voice.get_wav_data())
+    result = model.transcribe("english.wav", language= 'en', fp16=False)
+    print(result["text"])
+    promptText = result["text"]
+    promptText = promptText.lower()
+    return promptText             
+    
+      
+
+def render_in_gpt4all(text):
+    output = gptModel.generate(text, max_tokens=200)
+    print(output)
+    return output
+
 while True:
-    take_command()
+    userVoice = start_listening()
+    promptText = get_user_command_as_text(userVoice)
+    if wake_word in promptText:
+        gptOutput = render_in_gpt4all(promptText)
+        speak(gptOutput)
+    else:
+        print(wake_word + " is not in command")
+
